625607ce91faf498ed39c65e8beb9f431fd0a8dc
RyanFairclough <rfairclough@all.space>
2024-07-11 17:05:26 +0100

    Second commit to ryandev.

--- 20230412_EvalCalCheck/Pie_Yield_chart.py
+++ 20230412_EvalCalCheck/Pie_Yield_chart.py
@@ -2,28 +2,28 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-Failed_Folder_Name = "Failed"
+Failed_Folder_Name = r'C:\Users\RyanFairclough\ALL.SPACE\Engineering - P-Type\Tx_TLM_Rev_175-0182_v1\TLM_Calibration_Measurements\Alg2'
 colours = ("green", "red")
 labels = ["PASS", "Fail"]
 
-# Get the list of all files and directories in the specified path
-path = r'C:\Users\RyanFairclough\ALL.SPACE\Engineering - P-Type\Tx_TLM_Rev_175-0182_v1\TLM_Rework\NC-126\Post_rework_eval'
+
+path = r'C:\Users\RyanFairclough\ALL.SPACE\Engineering - P-Type\Tx_TLM_Rev_175-0182_v1\TLM_Calibration_Measurements\Alg2\Batch_3_For_P2\Batch_3_For_P2_BB\Raw_Data'
 dir_list = os.listdir(path)
 
-# Filter the list to include only items that start with "2024"
+
 filtered_list = [value for value in dir_list if value.startswith("2024")]
 print("Filtered List:", filtered_list)
 
-# Define the path to the "archive" subdirectory
-archive_path = os.path.join(path, Failed_Folder_Name)
 
-# Check if the "archive" directory exists
-if os.path.isdir(archive_path):
-    # Get the list of files in the "archive" directory
-    archive_files = os.listdir(archive_path)
-    print("Files in 'archive' directory:", archive_files)
+Failed_path = os.path.join(path, Failed_Folder_Name)
+
+
+if os.path.isdir(Failed_path):
+
+    archive_files = os.listdir(Failed_path)
+    print("Files in 'Failed' directory:", archive_files)
 else:
-    print("The 'archive' directory does not exist.")
+    print("The 'Failed' directory does not exist.")
     archive_files = []
 
 print("Passed_TLMs_in_Folder:", len(filtered_list))
@@ -45,4 +45,4 @@ def make_autopct(values):
 ax.pie(y, autopct=make_autopct(y), labels=labels, colors=colours)
 plt.show()
 
-fig.savefig(r'C:/Users/RyanFairclough/ALL.SPACE/Engineering - P-Type/Tx_TLM_Rev_175-0182_v1/TLM_Rework\NC-126/Post_rework_eval/pie.png')
+fig.savefig(r'C:\Users\RyanFairclough\ALL.SPACE\Engineering - P-Type\Tx_TLM_Rev_175-0182_v1\TLM_Calibration_Measurements\Alg2\Batch_3_For_P2\Batch_3_For_P2_BB\Raw_Data/pie.png')
